0608c92311e313987807a477a6a84e872cd6b34d
Xuanita <alabama10@inbox.lv>
2026-04-21 18:47:50 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const reversedStr = Math.abs(n).toString().split('').reverse().join('');
+  const reversedNumber = parseInt(reversedStr, 10);
+
+  return reversedNumber;
 };
